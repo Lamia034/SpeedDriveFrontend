7a88e561e76216b1c98c2d20c5434162f14ecb3a
Lamia BOURICH <lamiascience034@gmail.com>
2024-03-27 10:27:43 +0000

    final

--- src/app/authguard/AuthGuard.ts
+++ src/app/authguard/AuthGuard.ts
@@ -14,17 +14,11 @@ import {SharedService} from "../shared.service";
 
 
 export class AuthGuard implements CanActivate {
-  private userRolesSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
-  public userRoles$: Observable<string[]> = this.userRolesSubject.asObservable();
-
-  private lastRoles: string[] = [];
 
   constructor(
     private router: Router,
     private jwtHelper: JwtHelperService,
-    private loginService: LoginService,
     private sharedService: SharedService
-    // private authService: AuthService
   ) {}
 
 
@@ -34,22 +28,18 @@ export class AuthGuard implements CanActivate {
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     console.log('isLoggedIn in AuthGuard:', this.sharedService.isLoggedIn);
 this.sharedService.isLoggedIn;
-    console.log('canActivate is called');
+    console.log('canactivate is called');
     const allowedRoles = route.data['allowedRoles'];
     const token = localStorage.getItem('speedDrive_authResponse');
     console.log('AuthResponse from localStorage:', token);
 this.setRoles(token);
-    // Get user's roles from the SharedService
     const userRoles = this.sharedService.getRole();
     console.log('User roles:', userRoles);
 
-    // Check if user has any of the allowed roles
     if (userRoles && allowedRoles.some((role: string) => userRoles.includes(role.toUpperCase()))) {
-      console.log('User has allowed role');
-      return true; // User has the required role
+      return true;
     } else {
-      console.log('Redirecting to login');
-      return this.router.createUrlTree(['/login']); // Redirect to login page
+      return this.router.createUrlTree(['/login']);
     }
   }
 
@@ -76,89 +66,7 @@ this.setRoles(token);
 
       this.sharedService.setUserName(userName);
     }}
-    //   canActivate(
-    //     route: ActivatedRouteSnapshot,
-    //     state: RouterStateSnapshot
-    // ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    //     console.log('canActivate is called');
-    //     const allowedRoles = route.data['allowedRoles'];
-    //     if (this.sharedService.getRole() && allowedRoles.some((role: string) => this.sharedService.getRole().some((userRole: UserRole) => userRole.authority === role.toUpperCase()))) {
-    //       console.log('User has allowed role');
-    //       return true;
-    //     }
-    //
-    //
-    //     console.log('Redirecting to login');
-    //     return this.router.createUrlTree(['/login']);
-    //   }
-    //
 
-
-
-
-
-// // console.log("user name in auth gard after set :",this.loginService.getUserName());
-//       if (!this.areRolesEqual(userRoles, this.lastRoles)) {
-//
-//         this.userRolesSubject.next(userRoles);
-//         this.lastRoles = userRoles;
-//       }
-//     }
-//   }
-
-
-  // hasRole(role: string): Observable<boolean> {
-  //   return this.userRoles$.pipe(
-  //     map((userRoles: string | string[]) => {
-  //       if (Array.isArray(userRoles)) {
-  //         return userRoles.includes(role.toUpperCase());
-  //       }
-  //       return false;
-  //     })
-  //   );
-  // }
-  //
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot
-  // ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   console.log('canActivate is called');
-  //   const token = localStorage.getItem('authResponse');
-  //   console.log('AuthResponse from localStorage:', token);
-  //
-  //   this.setRoles(token);
-  //
-  //   return this.hasRole('AGENCY').pipe(
-  //     tap(hasAgencyRole => {
-  //       if (hasAgencyRole) {
-  //         console.log('User has AGENCY role. Redirecting to dashboard.');
-  //         this.router.navigate(['/dashboard']);
-  //       } else {
-  //         this.hasRole('CLIENT').subscribe(hasClientRole => {
-  //           if (hasClientRole) {
-  //             console.log('User has CLIENT role. Redirecting to profile.');
-  //             this.router.navigate(['/profile']);
-  //           } else {
-  //             console.log('User does not have AGENCY or CLIENT role. Redirecting to login.');
-  //             this.router.navigate(['/login']);
-  //           }
-  //         });
-  //       }
-  //     }),
-  //     catchError(error => {
-  //       console.error('Error checking roles:', error);
-  //       return of(false);
-  //     })
-  //   );
-  // }
-  //
-  // private areRolesEqual(roles1: string[] | undefined, roles2: string[] | undefined): boolean {
-  //   if (!roles1 || !roles2) {
-  //     return roles1 === roles2;
-  //   }
-  //
-  //   return roles1.length === roles2.length && roles1.every((role, index) => role === roles2[index]);
-  // }
 }
 
 
